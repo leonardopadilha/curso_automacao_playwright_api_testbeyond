7f52ddfb0ead769a5dccc0f8e2f11c65c7a7c0c2
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-10-14 07:08:16 -0300

    Usuário não deve ser cadastrado quando já existir o email na base de dados

--- playwright/e2e/auth/register.spec.js
+++ playwright/e2e/auth/register.spec.js
@@ -24,4 +24,28 @@ test.describe('POST /auth/register', () => {
        expect(responseBody.user.email).toBe(user.email)
        expect(responseBody.user).not.toHaveProperty('password')
     })
+
+    test('não deve cadastrar quando o email já estiver em uso', async ({ request }) => {
+        const firstName = faker.person.firstName()
+        const lastName = faker.person.lastName()
+
+        const user = {
+            name: `${firstName} ${lastName}`,
+            email: faker.internet.email({ firstName, lastName }).toLowerCase(),
+            password: 'pwd123'
+        }
+
+       const preCondition =  await request.post('http://localhost:3333/api/auth/register', {
+        data: user
+       })
+       expect(preCondition.status()).toBe(201)
+
+       const response =  await request.post('http://localhost:3333/api/auth/register', {
+        data: user
+       })
+       expect(response.status()).toBe(400)
+
+       const responseBody = await response.json()
+       expect(responseBody.message).toBe('Este e-mail já está em uso. Por favor, tente outro.')
+    })
 })
