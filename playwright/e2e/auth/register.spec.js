800626ee29811dfac409f2b1b1a22500a3aca690
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-10-14 07:09:03 -0300

    Sistema não deve permitir cadastro de usuário com email no formato inválido

--- playwright/e2e/auth/register.spec.js
+++ playwright/e2e/auth/register.spec.js
@@ -48,4 +48,20 @@ test.describe('POST /auth/register', () => {
        const responseBody = await response.json()
        expect(responseBody.message).toBe('Este e-mail já está em uso. Por favor, tente outro.')
     })
+
+    test('não deve cadastrar quando o email estiver com formato inválido', async ({ request }) => {
+        const user = {
+            name: `Leonardo Padilha`,
+            email: `leonardo*email.com`,
+            password: 'pwd123'
+        }
+
+       const response =  await request.post('http://localhost:3333/api/auth/register', {
+        data: user
+       })
+       expect(response.status()).toBe(400)
+
+       const responseBody = await response.json()
+       expect(responseBody).toHaveProperty("message", "O campo 'Email' deve ser um email válido")
+    })
 })
